103d9714afc67844cf9861bddea3addbe398691a
thejackshelton <me@jackshelton.com>
2025-11-07 13:38:39 -0600

    ini

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,35 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Carousel } from "@qwik-ui/headless";
 
 export default component$(() => {
+  const items = [
+    {
+      id: 1,
+      title: "Item 1",
+      description: "Description 1",
+    },
+    {
+      id: 2,
+      title: "Item 2",
+      description: "Description 2",
+    },
+    {
+      id: 3,
+      title: "Item 3",
+      description: "Description 3",
+    },
+  ];
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
-    </>
+    <Carousel.Root>
+      <Carousel.Scroller>
+        {items.map((item) => (
+          <Carousel.Slide key={item.id}>
+            <div>{item.title}</div>
+          </Carousel.Slide>
+        ))}
+      </Carousel.Scroller>
+    </Carousel.Root>
   );
 });
 
